8b2fba5f71496bb65cd47d4aee913f5f8332e898
Fluva74 <85516975+Fluva74@users.noreply.github.com>
2022-01-23 21:56:59 -0600

    error handling

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import GithubIcon from '@material-ui/icons/Github';
+
 import TwitterIcon from '@material-ui/icons/Twitter';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import Grid from '@material-ui/core/Grid';
@@ -8,7 +8,7 @@ import Link from '@material-ui/core/Link'
 
 
 const socialItems = [
-    { icon: GithubIcon, url: "https://github.com/Fluva74" },
+    
     { icon: TwitterIcon, url: "https://twitter.com/MikeFin54739502" },
     { icon: LinkedInIcon, url: "https://www.linkedin.com/in/mike-finnegan-596a96225/"}
 ]
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createTheme, ThemeProvider, responsiveFontSizes } from '@material-ui/core/styles';
-import CssBaseLine from '@material-ui/core/CssBaseLine'
+
 import HeroSection from '../components/HeroSection';
 import Header from '../components/Header';
 import Projects from '../components/Projects';
@@ -16,7 +16,7 @@ darkTheme = responsiveFontSizes(darkTheme)
 
 export default function indexPage() {
   return <ThemeProvider theme={darkTheme}>
-    <CssBaseLine />
+    
     <Header />
     <HeroSection />
     <Projects />
