d94865cc43b9b26ab1cd60ea43b8efb648e88c7b
Fluva74 <85516975+Fluva74@users.noreply.github.com>
2022-01-24 19:17:56 -0600

    new mui trials

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/ToolBar';
+import Toolbar from '@mui/material/Toolbar';
 import Container from '@material-ui/core/Container';
 import Avatar from '@material-ui/core/Avatar';
 import Hidden from '@material-ui/core/Hidden';
